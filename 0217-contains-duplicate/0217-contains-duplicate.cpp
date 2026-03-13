a3df59206606513e10ab70e9073b212d4cdd7785
Amar9405 <amarsonawane940567@gmail.com>
2026-03-13 15:27:22 +0530

    Time: 31 ms (75.78%), Space: 71 MB (88.61%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+       
+
+      //Brute force apporoach
+        // int n=nums.size();
+
+        // for(int i=0; i<n; i++){
+        //     for(int j=i+1; j<n; j++){
+        //         if(nums[i]==nums[j]){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+
+        sort(nums.begin(),nums.end());//O(n log n)
+
+        int n=nums.size();
+
+        for(int i=1; i<n; i++){//o(n)
+            if(nums[i-1] == nums[i]){
+                return true;
+            }
+        }
+
+        return false;
+        
+    }
+};

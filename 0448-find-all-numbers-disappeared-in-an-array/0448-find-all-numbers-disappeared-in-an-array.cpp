4d3e33f449b77645dd320a4edcddcf26d5b1b65b
Amar9405 <amarsonawane940567@gmail.com>
2026-03-15 22:47:30 +0530

    Time: 8 ms (40.78%), Space: 52.9 MB (98.17%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
 
-        vector<int> ans;
+        vector<int> ans;//space complexity(O(n)
         int n=nums.size();
 
 
-        for(int i=0; i<n; i++){
+        for(int i=0; i<n; i++){//O(n)
             int idx=abs(nums[i])-1;
 
             if(nums[idx] < 0){
@@ -15,7 +15,7 @@ public:
                 nums[idx]*=-1;
         }
 
-        for(int i=0; i<n; i++){
+        for(int i=0; i<n; i++){//O(n)
             if(nums[i] >  0){
                 ans.push_back(i+1);
             }
@@ -23,6 +23,7 @@ public:
 
         return ans;
       
-        
+        //total time complexity is O(n)+O(n)=O(n)
+        //space complexity is O(n)
     }
 };

99b0652b9385f2dcd1c32882cc94b189550f292b
Amar Ramdas Sonawane <amarsonawane940567@gmail.com>
2026-03-20 19:09:03 +0530

    Time: 0 ms (100%), Space: 22.8 MB (18.29%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+//    set<int> ss; // O(n log n )
+//    int n=nums.size();
+//    for(int i=0; i<n; i++){
+//         ss.insert(nums[i]); 
+//    }
+//    int index=0;
+//    for(auto st :ss){ //O(n)
+//       nums[index]=st;
+//       index++;
+//    }
+//       return index; 
 
+//space complexity = O(n) for storing unique element 
+//time complexity = O(n log n)+ O(n);
 
-   set<int> ss;
    int n=nums.size();
-
-   for(int i=0; i<n; i++){
-        ss.insert(nums[i]); 
-   }
-   int index=0;
-
-   for(auto st :ss){
-      nums[index]=st;
-      index++;
+   int i=0;
+   for(int j=i+1; j<n; j++){
+       if(nums[j]!=nums[i]){
+         nums[i+1]=nums[j];
+         i++;
+       }
    }
 
-   return index;
+   return i+1;
 
-        
+   // time complexity of this program is O(n) and sp=O(1).
+
+
     }
 };

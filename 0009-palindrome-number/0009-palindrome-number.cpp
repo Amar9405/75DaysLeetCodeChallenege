12ce3969008c33575a538c696ba01f69575f2c9d
Amar Ramdas Sonawane <amarsonawane940567@gmail.com>
2026-03-21 20:26:49 +0530

    Time: 3 ms (44.91%), Space: 8.7 MB (22.05%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+       if(x < 0){
+        return false;
+       } 
+       int original=x;
+       long long ans=0;
+       while(x > 0){
+           int lastdigit = x % 10;
+           ans = ans * 10 + lastdigit;
+           x = x / 10 ;
+       }
+
+
+       return ans==original;
+         
+       
+    }
+};

0466e0eac7dbd9e99468e8352b6020a4c58d3b66
Amar Ramdas Sonawane <amarsonawane940567@gmail.com>
2026-03-22 20:28:05 +0530

    Time: 3 ms (30.41%), Space: 9.9 MB (77.44%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,31 +1,45 @@
 class Solution {
 public:
 
-    bool isAlpha(char ch){
-        if((ch >= '0' && ch <= '9') || (tolower(ch) >= 'a' && tolower(ch) <= 'z')){
-            return true;
-        }
-        return false;
-    }
+   bool isAlpha(char ch){
+      if(ch >= '0' && ch <= '9' || tolower(ch) >='a' && tolower(ch) <='z'){
+                  return true;
+      }
+
+      return false;
+   }
+
     bool isPalindrome(string s) {
+
+
         int n=s.size();
-        int st=0;
-        int end=n-1;
-       while(st < end){
-          if(!isAlpha(s[st])){
-              st++;
-              continue;
-          }
-          if(!isAlpha(s[end])){
-              end--;
-              continue;
-          }
-          if(tolower(s[st]) != tolower(s[end])){
-              return false;
-          }
-          st++;
-          end--; 
-       }
-       return true;
+        int i=0;
+        int j=n-1;
+
+        while(i < j){
+            if(!isAlpha(s[i])){  
+                i++;
+                continue;
+            }
+
+             if(!isAlpha(s[j])){  
+                j--;
+                continue;
+            }
+
+            if( tolower(s[i]) != tolower(s[j])){
+                return false;
+            }
+
+            i++;
+            j--;
+
+        }
+
+
+        return true;
+
+
+        
     }
 };

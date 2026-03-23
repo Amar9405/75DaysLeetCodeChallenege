a3f92c086a2153d9f86647ff35b54e7798bcccff
Amar Ramdas Sonawane <amarsonawane940567@gmail.com>
2026-03-23 13:55:15 +0530

    Time: 51 ms (45.44%), Space: 31 MB (21.33%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-
         vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
         int n=nums.size();
 
         for(int i = 0 ; i < n; i++ ){
             if(i > 0 && nums[i]== nums[i-1]) continue;
-
             int j=i+1;
             int k=n-1;
-
             while( j < k ){
                 int  sum=nums[i]+nums[j]+nums[k];
-
                 if(sum > 0){
                     k--;
                 }else if(sum < 0){
@@ -25,8 +21,7 @@ public:
                     j++;
                     k--;
                     while( j < k && nums[j]==nums[j-1]) j++;
-                    while(j < k && nums[k]==nums[k+1]) k--;
-                
+                    while(j < k && nums[k]==nums[k+1]) k--; 
 
             }
 
@@ -35,6 +30,9 @@ public:
     }
 
         return ans;
+
+        //time complexity of this program = O(O(n log n)+O(n)+0(n));
+        //space complexity =0(number of unique triplete)
         
     }
 };

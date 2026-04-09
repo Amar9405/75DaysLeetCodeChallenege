0e4627afde0ce9dabc9099b0d8f50943ac1df543
Amar Ramdas Sonawane <amarsonawane940567@gmail.com>
2026-04-10 00:32:21 +0530

    Time: 0 ms (100%), Space: 19.4 MB (61.71%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,39 +11,62 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        vector<int> arr;
+    //     vector<int> arr;
 
-        ListNode* temp=list1;
+    //     ListNode* temp=list1;
 
-        while(temp != NULL){
-            arr.push_back(temp->val);
-            temp=temp->next;
-        }
+    //     while(temp != NULL){
+    //         arr.push_back(temp->val);
+    //         temp=temp->next;
+    //     }
 
-       ListNode* temp2=list2;
+    //    ListNode* temp2=list2;
 
-        while(temp2 != NULL){
-             arr.push_back(temp2->val);
-             temp2=temp2->next;
-        }
+    //     while(temp2 != NULL){
+    //          arr.push_back(temp2->val);
+    //          temp2=temp2->next;
+    //     }
 
-        sort(arr.begin(),arr.end());
+    //     sort(arr.begin(),arr.end());
 
-        if(arr.size()==0) return NULL;
-
-
-        ListNode* head=new ListNode(arr[0]);
-        ListNode* mover=head;
-
-        for(int i=1; i<arr.size(); i++){
-            ListNode* temp=new ListNode(arr[i]);
-            mover->next=temp;
-            mover=temp;
-        }
-       
+    //     if(arr.size()==0) return NULL;
 
 
-        return head;
+    //     ListNode* head=new ListNode(arr[0]);
+    //     ListNode* mover=head;
+
+    //     for(int i=1; i<arr.size(); i++){
+    //         ListNode* temp=new ListNode(arr[i]);
+    //         mover->next=temp;
+    //         mover=temp;
+    //     }
+    //     return head;
+    ListNode* t1=list1;
+    ListNode* t2=list2;
+
+    ListNode* dnode=new ListNode(-1);
+    ListNode* temp=dnode;
+    
+
+    while(t1 != NULL && t2 != NULL){
+         if(t1->val < t2->val){
+            temp->next=t1;
+            temp=t1;
+            t1=t1->next;
+         }else{// t2->val < t1->val
+            temp->next=t2;
+            temp=t2;
+            t2=t2->next;
+         }
+    }
+
+    if(t1==NULL) temp->next=t2;
+    if(t2==NULL) temp->next=t1;
+
+    
+
+    
+    return dnode->next;
 
 
     }

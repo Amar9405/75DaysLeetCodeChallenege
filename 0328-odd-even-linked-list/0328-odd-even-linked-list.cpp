8f4de37bef89c9381c18c72aec9fa1b3a9eb4522
Amar Ramdas Sonawane <amarsonawane940567@gmail.com>
2026-04-14 17:47:10 +0530

    Time: 0 ms (100%), Space: 15.7 MB (27.34%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -12,39 +12,60 @@ class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
 
-        if(head == NULL ||  head->next == NULL) return head;
+        // if(head == NULL ||  head->next == NULL) return head;
         
         
-         vector<int> arr;
-         ListNode* temp=head;
+        //  vector<int> arr;
+        //  ListNode* temp=head;
         
-        //odd place element put in array
-         while(temp != NULL && temp->next != NULL){
-            arr.push_back(temp->val);
-            temp=temp->next->next;
-         }
+        // //odd place element put in array
+        //  while(temp != NULL && temp->next != NULL){
+        //     arr.push_back(temp->val);
+        //     temp=temp->next->next;
+        //  }
 
-        if(temp != NULL) arr.push_back(temp->val);
+        // if(temp != NULL) arr.push_back(temp->val);
 
-         temp=head->next;
-         //even place element put in array
-         while(temp != NULL && temp->next != NULL ){
-            arr.push_back(temp->val);
-            temp=temp->next->next;
-         }
+        //  temp=head->next;
+        //  //even place element put in array
+        //  while(temp != NULL && temp->next != NULL ){
+        //     arr.push_back(temp->val);
+        //     temp=temp->next->next;
+        //  }
 
-         if(temp != NULL)  arr.push_back(temp->val);
+        //  if(temp != NULL)  arr.push_back(temp->val);
 
-         temp=head;
-         int i=0;
+        //  temp=head;
+        //  int i=0;
 
-         while(temp != NULL ){
-            temp->val=arr[i];
-            i++;
-            temp=temp->next;
-         }
+        //  while(temp != NULL ){
+        //     temp->val=arr[i];
+        //     i++;
+        //     temp=temp->next;
+        //  }
 
-         return head;
+        //  return head;  time complexity=O(2n) space complexity:-O(n)
+
+
+    if(head==NULL || head->next==NULL)  return head;
+    
+    ListNode* Odd=head;
+    ListNode* even=head->next;
+    ListNode* evenhead=head->next;
+
+    while(even != NULL && even->next != NULL){
+        Odd->next=Odd->next->next;
+        even->next=even->next->next;
+
+        Odd=Odd->next;
+        even=even->next;
+    }
+
+    Odd->next=evenhead;
+
+    return head;
+        
+
         
     }
 };

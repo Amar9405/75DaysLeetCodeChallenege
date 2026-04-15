c11dba3fd19342c0aa4bb849c47cdbd26298395e
Amar Ramdas Sonawane <amarsonawane940567@gmail.com>
2026-04-16 01:48:49 +0530

    Time: 0 ms (100%), Space: 312.1 MB (54.19%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -12,43 +12,63 @@ class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
         
-        if(head==NULL || head->next==NULL){
-            return NULL;
-        }
+    //     if(head==NULL || head->next==NULL){
+    //         return NULL;
+    //     }
 
-        if(head->next->next==NULL){
-            head->next=NULL;
-            return head;
-        }
+    //     if(head->next->next==NULL){
+    //         head->next=NULL;
+    //         return head;
+    //     }
         
-        ListNode* temp=head;
-        int cnt=0;
-        while(temp != NULL){
-            cnt++;
-            temp=temp->next;
-        }
+    //     ListNode* temp=head;
+    //     int cnt=0;
+    //     while(temp != NULL){
+    //         cnt++;
+    //         temp=temp->next;
+    //     }
 
-        int NodetoDelete=(cnt/2);
+    //     int NodetoDelete=(cnt/2);
 
-        temp=head;
-        ListNode* prev=head;
+    //     temp=head;
+    //     ListNode* prev=head;
         
-        cnt=-1;
+    //     cnt=-1;
 
 
-        while(temp != NULL){
-            cnt++;
-            if(cnt==NodetoDelete){
-               prev->next=temp->next;
-               temp->next=NULL;
-               delete temp;
-               break;
-            }
-            prev=temp;
-            temp=temp->next;
-        }
+    //     while(temp != NULL){
+    //         cnt++;
+    //         if(cnt==NodetoDelete){
+    //            prev->next=temp->next;
+    //            temp->next=NULL;
+    //            delete temp;
+    //            break;
+    //         }
+    //         prev=temp;
+    //         temp=temp->next;
+    //     }
 
-      return head;
+    //   return head;
+
+    if(head==NULL || head->next==NULL){
+        return NULL;
+    }
+
+    ListNode* slow=head;
+    ListNode* fast=head;
+    ListNode* prev=NULL;
+
+    while( fast !=NULL && fast->next !=NULL){
+        prev=slow;
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+
+    prev->next = slow->next;
+    delete slow;
+
+    return head;
+
 
     }
 };

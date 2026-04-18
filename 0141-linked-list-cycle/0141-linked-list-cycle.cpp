2be678abacb6c7e563fd2fab293754a3c444a442
Amar Ramdas Sonawane <amarsonawane940567@gmail.com>
2026-04-19 00:39:58 +0530

    Time: 12 ms (43.51%), Space: 11.9 MB (52.13%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,18 +9,33 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        map<ListNode* ,int> mpp;
+        // map<ListNode* ,int> mpp;
 
-        ListNode* temp=head;
+        // ListNode* temp=head;
 
-        while(temp != NULL){
-            if(mpp.find(temp) != mpp.end()){
-                return true;
-            }
-            mpp[temp]=1;
-            temp=temp->next;
+        // while(temp != NULL){
+        //     if(mpp.find(temp) != mpp.end()){
+        //         return true;
+        //     }
+        //     mpp[temp]=1;
+        //     temp=temp->next;
+        // }
+
+        // return false;  Time complexity :- O(n)+O(log n )//for map and loop
+
+        //space complexity :- O(n) we store the number of element in map DS
+
+        ListNode* slow=head;
+        ListNode* fast=head;
+
+        while(fast != NULL && fast->next != NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast) return true;
         }
 
         return false;
+
+
     }
 };

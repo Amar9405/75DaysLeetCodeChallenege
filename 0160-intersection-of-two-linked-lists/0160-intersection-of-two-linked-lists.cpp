85d5ce0e10e3d9cb8712504d42717409e5f96bff
Amar Ramdas Sonawane <amarsonawane940567@gmail.com>
2026-04-23 23:43:57 +0530

    Time: 59 ms (41.49%), Space: 23.9 MB (55.07%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -8,24 +8,62 @@
  */
 class Solution {
 public:
+  
+   
+   ListNode* collisionPoint(ListNode* temp1 ,ListNode* temp2,int d){
+       
+       while(d){
+         d--;
+         temp2=temp2->next;
+       }
+
+       while(temp1 != temp2){
+         temp1=temp1->next;
+         temp2=temp2->next;
+       }
+     return temp1;
+   }
+
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
 
-        ListNode* temp=headA;
-        map<ListNode*, int> mpp;
+        // ListNode* temp=headA;
+        // map<ListNode*, int> mpp;
 
-        while(temp != NULL){
-            mpp[temp]=1;
-            temp=temp->next;
-        }
-        temp=headB;
-        while(temp != NULL){
-            if(mpp.find(temp) != mpp.end()){
-                return temp;
-            }
-            temp=temp->next;
+        // while(temp != NULL){
+        //     mpp[temp]=1;
+        //     temp=temp->next;
+        // }
+        // temp=headB;
+        // while(temp != NULL){
+        //     if(mpp.find(temp) != mpp.end()){
+        //         return temp;
+        //     }
+        //     temp=temp->next;
+        // }
+        // return NULL;
+
+        ListNode* t1=headA;
+        int L1=0;
+        
+        while(t1 != NULL){
+            L1++;
+            t1=t1->next;
         }
 
-        return NULL;
+        ListNode* t2=headB;
+        int L2=0;
+
+        while(t2 != NULL){
+           L2++;
+           t2=t2->next; 
+        }
+
+        if(L1 < L2){
+            return collisionPoint(headA,headB,L2-L1);
+        }else{
+            return collisionPoint(headB,headA,L1-L2);
+        }
+
 
         
     }

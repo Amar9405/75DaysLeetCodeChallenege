bd125f7122ace01facf3e78496170c316e026bd0
Amar Ramdas Sonawane <amarsonawane940567@gmail.com>
2026-04-24 14:34:38 +0530

    Time: 47 ms (95.13%), Space: 23.9 MB (84.74%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -7,22 +7,19 @@
  * };
  */
 class Solution {
-public:
-  
-   
-   ListNode* collisionPoint(ListNode* temp1 ,ListNode* temp2,int d){
-       
-       while(d){
-         d--;
-         temp2=temp2->next;
-       }
+public:  
+//    ListNode* collisionPoint(ListNode* temp1 ,ListNode* temp2,int d){   
+//        while(d){
+//          d--;
+//          temp2=temp2->next;
+//        }
 
-       while(temp1 != temp2){
-         temp1=temp1->next;
-         temp2=temp2->next;
-       }
-     return temp1;
-   }
+//        while(temp1 != temp2){
+//          temp1=temp1->next;
+//          temp2=temp2->next;
+//        }
+//      return temp1;
+//    }
 
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
 
@@ -42,28 +39,46 @@ public:
         // }
         // return NULL;
 
-        ListNode* t1=headA;
-        int L1=0;
+        // ListNode* t1=headA;
+        // int L1=0;
         
-        while(t1 != NULL){
-            L1++;
-            t1=t1->next;
-        }
+        // while(t1 != NULL){
+        //     L1++;
+        //     t1=t1->next;
+        // }
 
+        // ListNode* t2=headB;
+        // int L2=0;
+
+        // while(t2 != NULL){
+        //    L2++;
+        //    t2=t2->next; 
+        // }
+
+        // if(L1 < L2){
+        //     return collisionPoint(headA,headB,L2-L1);
+        // }else{
+        //     return collisionPoint(headB,headA,L1-L2);
+        // }
+
+        //optimal apporoach
+
+        ListNode* t1=headA;
         ListNode* t2=headB;
-        int L2=0;
+        while(t1 != t2){
+            t1=t1->next;
+            t2=t2->next;
+           //this condition for only if both linked list is independent then t1 and t2 is point NULL;
+            if(t1 == t2)  return t1;
 
-        while(t2 != NULL){
-           L2++;
-           t2=t2->next; 
+            if(t1==NULL){
+                t1=headB;
+            }
+            if(t2==NULL){
+                t2=headA;
+            }
         }
-
-        if(L1 < L2){
-            return collisionPoint(headA,headB,L2-L1);
-        }else{
-            return collisionPoint(headB,headA,L1-L2);
-        }
-
+        return t1;
 
         
     }

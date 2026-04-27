1ea81d4540cfdfc807ef4604db301a93c84e5b52
Amar Ramdas Sonawane <amarsonawane940567@gmail.com>
2026-04-27 23:19:01 +0530

    Time: 0 ms (100%), Space: 124.3 MB (67.83%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -10,29 +10,72 @@
  */
 class Solution {
 public:
-    int pairSum(ListNode* head) {
+  
+    ListNode* middleofLL(ListNode* head){
+        ListNode* fast=head;
+        ListNode* slow=head;
 
-        vector<int> arr;
-
-        ListNode* temp=head;
-
-        while(temp != NULL){
-            arr.push_back(temp->val);
-            temp=temp->next;
+        while(fast != NULL){
+            slow=slow->next;
+            fast=fast->next->next;
         }
+        return slow;
+    }
 
-        int i=0;
-        int j=arr.size()-1;
+    ListNode* Reverse(ListNode* head){
+         ListNode* temp=head;
+         ListNode* prev=NULL;
+
+         while(temp != NULL){
+             ListNode*  next=temp->next;
+             temp->next=prev;
+             prev=temp;
+             temp=next;
+         }
+         return prev;
+    }
+
+   
+    int pairSum(ListNode* head) {
+        // vector<int> arr;
+
+        // ListNode* temp=head;
+
+        // while(temp != NULL){
+        //     arr.push_back(temp->val);
+        //     temp=temp->next;
+        // }
+
+        // int i=0;
+        // int j=arr.size()-1;
+        // int maxi=INT_MIN;
+
+        // while(i < j){
+        //      int sum=arr[i]+arr[j];
+        //      maxi=max(maxi,sum);
+        //      i++;
+        //      j--;
+        // }
+
+        // return maxi; Time complexity :- O(n)+O(n/2) space complexity=O(n);
+
+        ListNode* middleNodeLL=middleofLL(head);
+        ListNode* newReverse=Reverse(middleNodeLL);
+
+        ListNode* t1=head;
+        ListNode* t2=newReverse;
         int maxi=INT_MIN;
+        while(t2 != NULL){
+            int sum=(t1->val+t2->val);
+            maxi=max(maxi,sum);
 
-        while(i < j){
-             int sum=arr[i]+arr[j];
-             maxi=max(maxi,sum);
-             i++;
-             j--;
+            t1=t1->next;
+            t2=t2->next;
         }
 
         return maxi;
+
+
    
     }
 };

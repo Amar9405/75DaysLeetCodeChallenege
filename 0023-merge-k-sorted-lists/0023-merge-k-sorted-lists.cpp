20e258641fe0cbbd17c137e6e35748c004286c9c
Amar Ramdas Sonawane <amarsonawane940567@gmail.com>
2026-05-01 20:01:30 +0530

    Time: 106 ms (8.84%), Space: 18.3 MB (92.13%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -11,17 +11,43 @@
 class Solution {
 public:
    
-   ListNode* convert(vector<int>& arr){
+//    ListNode* convert(vector<int>& arr){
          
-          ListNode* head=new ListNode(arr[0]);
-          ListNode* mover=head;
+//           ListNode* head=new ListNode(arr[0]);
+//           ListNode* mover=head;
 
-          for(int i=1; i<arr.size();i++){
-               ListNode* temp=new ListNode(arr[i]);
-               mover->next=temp;
-               mover=temp;    
-          }
-          return head;
+//           for(int i=1; i<arr.size();i++){
+//                ListNode* temp=new ListNode(arr[i]);
+//                mover->next=temp;
+//                mover=temp;    
+//           }
+//           return head;
+//    }
+
+   ListNode* Merge2Lists(ListNode* l1, ListNode* l2){
+       ListNode* dummyNode=new ListNode(-1);
+       ListNode* mover=dummyNode;
+       ListNode* t1=l1;
+       ListNode* t2=l2;
+
+
+       while(t1 != NULL && t2 != NULL){
+           if(t1->val < t2->val){
+              mover->next=t1;
+              t1=t1->next;
+              mover=mover->next;
+           }else{//t2->val < t1->val;
+              mover->next=t2;
+              t2=t2->next;
+              mover=mover->next;
+           }
+       }
+    
+       if(t1==NULL) mover->next=t2;
+       if(t2==NULL) mover->next=t1;
+
+       return dummyNode->next;
+
    }
 
 
@@ -29,30 +55,36 @@ public:
 
     ListNode* mergeKLists(vector<ListNode*>& lists) {
      
-      int size=lists.size();
-      vector<int> helper;
+    //   int size=lists.size();
+    //   vector<int> helper;
 
-      for(int i=0; i<size; i++){         
-          ListNode* temp=lists[i];
+    //   for(int i=0; i<size; i++){         
+    //       ListNode* temp=lists[i];
 
-          while(temp != NULL){
-             helper.push_back(temp->val);
-             temp=temp->next;
-          }
-      }
+    //       while(temp != NULL){
+    //          helper.push_back(temp->val);
+    //          temp=temp->next;
+    //       }
+    //   }
 
-      sort(helper.begin(),helper.end());
+    //   sort(helper.begin(),helper.end());
 
-      if(helper.size()==0) return NULL;
+    //   if(helper.size()==0) return NULL;
 
-      ListNode* headNode=convert(helper);
-
-
-      return headNode;
-        
-       
+    //   ListNode* headNode=convert(helper);
 
 
-        
+    //   return headNode;
+
+    if(lists.size()==0) return NULL;
+
+    ListNode* LinkList1=lists[0];
+
+    for(int i=1; i<lists.size();i++){
+        ListNode* LinkList2=lists[i];
+        LinkList1=Merge2Lists(LinkList1,LinkList2);
+     }
+
+        return LinkList1;   
     }
 };

bf0747993e3d8a0e160eb1c0eb4bf4c6af113fe1
Amar Ramdas Sonawane <amarsonawane940567@gmail.com>
2026-05-04 19:50:26 +0530

    Time: 49 ms (55.81%), Space: 90.9 MB (27.79%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,43 +1,18 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-       
+        
+        unordered_set<int> mpp;
 
-      //Brute force apporoach
-        // int n=nums.size();
-
-        // for(int i=0; i<n; i++){
-        //     for(int j=i+1; j<n; j++){
-        //         if(nums[i]==nums[j]){
-        //             return true;
-        //         }
-        //     }
-        // }
-        // return false;
-
-        // sort(nums.begin(),nums.end());//O(n log n)
-
-        // int n=nums.size();
-
-        // for(int i=1; i<n; i++){//o(n)
-        //     if(nums[i-1] == nums[i]){
-        //         return true;
-        //     }
-        // }
-
-        // return false;
-
-        //most optimal solution 
-
-        int n=nums.size();
-        unordered_set<int> seen;
-
-        for (int i=0;i<n; i++){
-              if(seen.find(nums[i])!= seen.end()){                    
-                            return true;
-                }
-            seen.insert(nums[i]);   //time complexity O(n)  space complexity:-O(n)
+        for(int i=0; i<nums.size(); i++){
+            if(mpp.find(nums[i]) != mpp.end()){
+                return true;
+            }
+            mpp.insert(nums[i]);
         }
-    return false;       
+
+         return false;
+
+        
     }
 };

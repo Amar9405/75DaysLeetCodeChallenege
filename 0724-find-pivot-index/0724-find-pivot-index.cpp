d6b9095250d4a0edba8009b7f3555974946e0667
Amar Ramdas Sonawane <amarsonawane940567@gmail.com>
2026-05-09 13:23:23 +0530

    Time: 0 ms (100%), Space: 35.6 MB (78.2%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+
+     
+      int rightsum=0;
+
+      for(int num:nums){
+         rightsum+=num;
+      }
+
+      int leftsum=0;
+
+      for(int i=0; i<nums.size(); i++){
+    
+
+         rightsum-=nums[i];
+
+         if(rightsum==leftsum){
+            return i;
+         }
+
+         leftsum+=nums[i];
+
+      }
+
+      return -1;
+
+
+
+
+
+       
+        
+    }
+};

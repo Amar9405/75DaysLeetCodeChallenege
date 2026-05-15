218eda88484ade8a5145f0cffc42dd10a0c31c9e
Amar Ramdas Sonawane <amarsonawane940567@gmail.com>
2026-05-15 13:15:36 +0530

    Time: 0 ms (100%), Space: 8.7 MB (94.03%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -2,15 +2,21 @@ class Solution {
 public:
 
     vector<int> getRow(int n) {
-         long long res=1;
-         vector<int> ans;
+         
+         //store the each colus in this ans vector
+         vector<int> ans ;
 
-         for(int i=0; i<=n; i++){
-            ans.push_back(res);
-            res=res* (n-i) /(i+1);
+
+         long long res=1;
+
+         for(int i=0; i<=n;  i++){
+             ans.push_back(res);
+             res=res*(n-i) /(i+1); 
          }
 
          return ans;
+
+
     
     }
 };

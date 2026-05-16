7b61c3185d4b2171aa87d48bfcfc34acc38a52ff
Amar Ramdas Sonawane <amarsonawane940567@gmail.com>
2026-05-16 17:04:16 +0530

    Time: 0 ms (100%), Space: 22.4 MB (83.27%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.cpp
+++ 0498-diagonal-traverse/0498-diagonal-traverse.cpp
@@ -4,8 +4,8 @@ public:
 
         if(mat.empty() || mat[0].empty()) return {};
 
-        int m=mat.size();
-        int n=mat[0].size();
+        int n=mat.size();
+        int m=mat[0].size();
         
         vector<int> ans(n*m);
         int row=0;
@@ -16,11 +16,11 @@ public:
             ans[i]=mat[row][col];
               //upword 
             if((row+col)%2==0){
-               if(col == n-1) row++;
+               if(col == m-1) row++;
                else if(row == 0) col++;
                else {row--; col++;};
             }else{//downword
-              if(row == m-1) col++;
+              if(row == n-1) col++;
               else if(col == 0) row++;
               else {row++; col--; };              
             }                    

300440848bad93aa5fea59869338b6fd1bcbc3ea
Amar Ramdas Sonawane <amarsonawane940567@gmail.com>
2026-05-20 13:22:52 +0530

    Time: 71 ms (28.17%), Space: 167.7 MB (13.52%) - LeetHub

--- 2055-plates-between-candles/2055-plates-between-candles.cpp
+++ 2055-plates-between-candles/2055-plates-between-candles.cpp
@@ -2,39 +2,105 @@ class Solution {
 public:
     vector<int> platesBetweenCandles(string s, vector<vector<int>>& queries) {
         
-        vector<int> barIndex;
+        // vector<int> barIndex;
 
-        for(int i=0; i<s.size(); i++){
+        // for(int i=0; i<s.size(); i++){
 
-            if(s[i] == '|'){
-                barIndex.push_back(i);
-            }
-        }
+        //     if(s[i] == '|'){
+        //         barIndex.push_back(i);
+        //     }
+        // }
 
         
-        vector<int> ans;
+        // vector<int> ans;
 
-        for(auto it : queries){
-            int left=it[0];
-            int right=it[1];
+        // for(auto it : queries){
+        //     int left=it[0];
+        //     int right=it[1];
 
-            int i = lower_bound(barIndex.begin(), barIndex.end(), left) - barIndex.begin();
+        //     int i = lower_bound(barIndex.begin(), barIndex.end(), left) - barIndex.begin();
 
-            int j = upper_bound(barIndex.begin(), barIndex.end(), right) - barIndex.begin() - 1;
+        //     int j = upper_bound(barIndex.begin(), barIndex.end(), right) - barIndex.begin() - 1;
 
-           if(i >= j){
-                ans.push_back(0);
+        //    if(i >= j){
+        //         ans.push_back(0);
+        //     }
+        //     else{
+
+        //         int plates = (barIndex[j] - barIndex[i]) - (j - i);
+
+        //         ans.push_back(plates);
+
+        //   }
+        // }
+
+        // return ans;
+
+        int n=s.size();
+
+        vector<int> leftcandle(n);
+        vector<int> rightcandle(n);
+        vector<int> prefix(n);
+
+
+        //nearest candle on right side
+
+        int last=-1;
+        for(int i=0; i<n; i++){
+            if(s[i]=='|'){
+                last=i;
             }
-            else{
 
-                int plates = (barIndex[j] - barIndex[i]) - (j - i);
+            leftcandle[i] = last;
 
-                ans.push_back(plates);
-
-          }
         }
 
-        return ans;
+
+        last=-1;
+
+        //nearest candle on right side
+
+        for(int i=n-1;  i >= 0; i--){
+            
+            if(s[i]=='|'){
+                last=i;
+            }
+
+             rightcandle[i]=last;
+
+        }
+
+        prefix[0]=(s[0]=='*');
+
+        for(int i=1;  i<n; i++){
+
+         prefix[i]= prefix[i-1] + (s[i] == '*');
+
+        }
+
+        vector<int> ans;
+
+       for(auto it : queries){
+
+        int left=rightcandle[it[0]];
+        int right=leftcandle[it[1]];
+       
+
+        if(left == -1 || right==-1 || left >=  right){
+            ans.push_back(0);
+        }else{
+           
+           int plates_between=prefix[right]-prefix[left];
+           ans.push_back(plates_between);
+
+        }
+            
+       }
+
+       return ans;
+
+
+
 
 
     }

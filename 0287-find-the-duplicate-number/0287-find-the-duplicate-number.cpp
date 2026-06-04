b8bb07a1ed059aad65a636cf857c17a0b3ecea2f
Amar Ramdas Sonawane <amarsonawane940567@gmail.com>
2026-06-04 15:50:26 +0530

    Time: 33 ms (29.47%), Space: 65 MB (90.4%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+
+         int n= nums.size();
+
+         sort(nums.begin(),nums.end()); 
+
+         int ans=0;
+
+         for(int i=0; i<n; i++){
+             if(nums[i]==nums[i+1]){
+                ans=nums[i];
+                break; 
+             }
+         }
+
+         return ans;
+        
+    }
+};

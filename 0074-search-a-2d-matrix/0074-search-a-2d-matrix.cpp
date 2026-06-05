c252d2bfa82e23f78be39629280559eb7139e40c
Amar Ramdas Sonawane <amarsonawane940567@gmail.com>
2026-06-06 00:12:44 +0530

    Time: 0 ms (100%), Space: 13.5 MB (6.99%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,41 +1,39 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size(), n = matrix[0].size();
-        
-        int startrow = 0, endrow = m - 1;
-        
-        // Binary search on rows
-        while (startrow <= endrow) {
-            int midrow = startrow + (endrow - startrow) / 2;
-            
-            // Check the first and last elements of the mid row
-            if (matrix[midrow][0] <= target && matrix[midrow][n-1] >= target) {  // Now, we know the target could be in this row, do binary search on the row
-                int startcol = 0, endcol = n - 1;
-                
-                while (startcol <= endcol) {
-                    int midcol = startcol + (endcol - startcol) / 2;
-                    
-                    if (matrix[midrow][midcol] == target) {
-                        return true;
-                    }
-                    else if (matrix[midrow][midcol] < target) {
-                        startcol = midcol + 1;
-                    }
-                    else {
-                        endcol = midcol - 1;
-                    }
-                }
-                return false;  // If we exit this loop, the target is not found in the row
+    
+    bool binarySearch(vector<int> mat , int tar){
+         int n= mat.size();
+         int low =0;
+         int high=n-1;
+
+         while(low <= high){
+            int mid=(low+high)/2;
+
+            if(mat[mid]==tar){
+                return true;
+            }else if(tar > mat[mid]){
+                  low=mid+1;
+            }else{//traget < mat[mid]
+                high=mid-1;
+
             }
-            else if (matrix[midrow][n-1] < target) {
-                startrow = midrow + 1;
-            }
-            else {
-                endrow = midrow - 1;
+         }
+         return false;
+
+    }
+ 
+    bool searchMatrix(vector<vector<int>>& mat, int target) {
+        //Better approach using binary search.
+
+        int n=mat.size();
+        int m=mat[0].size();
+
+        for(int i=0; i<n; i++){
+
+            if(mat[i][0] <= target && target <= mat[i][m-1]){
+                return binarySearch(mat[i],target);
             }
         }
-        
-        return false;  // If no row contains the target, return false
+        return false;
     }
 };

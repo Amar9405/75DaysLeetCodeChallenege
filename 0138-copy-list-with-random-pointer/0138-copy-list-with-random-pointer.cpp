f4aa262466e8205a4465f4eb0afa12b1b9e328f8
Amar Ramdas Sonawane <amarsonawane940567@gmail.com>
2026-06-13 23:15:26 +0530

    Time: 7 ms (51.29%), Space: 15 MB (70.41%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -18,42 +18,91 @@ class Solution {
 public:
     Node* copyRandomList(Node* head) {
 
-        if(head==NULL){
-            return NULL;
-        }
+        // if(head==NULL){
+        //     return NULL;
+        // }
 
 
 
-        //step 1:- copy the each node of linked list in hashmap;
+        // //step 1:- copy the each node of linked list in hashmap;
 
-         Node* temp=head;
-         unordered_map<Node*, Node*> mpp;
+        //  Node* temp=head;
+        //  unordered_map<Node*, Node*> mpp;
 
-         while(temp != NULL){
-            Node* newNode= new Node(temp->val);
+        //  while(temp != NULL){
+        //     Node* newNode= new Node(temp->val);
 
-            mpp[temp]=newNode;
+        //     mpp[temp]=newNode;
 
-            temp=temp->next;
-         }
+        //     temp=temp->next;
+        //  }
 
-         //step 2 the Link  the next and random pointer to currect node.
+        //  //step 2 the Link  the next and random pointer to currect node.
 
-         temp=head;
+        //  temp=head;
 
-         while(temp != NULL){
+        //  while(temp != NULL){
               
-             Node* copynode=mpp[temp];
+        //      Node* copynode=mpp[temp];
 
-            copynode->next=mpp[temp->next];
+        //     copynode->next=mpp[temp->next];
 
-            copynode->random=mpp[temp->random];
+        //     copynode->random=mpp[temp->random];
 
+        //     temp=temp->next;
+
+        //  }
+
+        //  return mpp[head];
+
+
+         //most Optimal solutiong
+        
+        //step 1 :- create copy of the each node and place theat copy in between .
+        
+        Node* temp=head;
+        
+        while(temp != NULL){
+            Node* copynode=new Node(temp->val);
+            copynode->next=temp->next;
+            temp->next=copynode;
+            temp=temp->next->next;
+        }
+        
+        
+        //step2 connect the  random pointer .
+        
+        temp=head;
+        
+        while(temp != NULL){
+            Node* copynode=temp->next;
+            
+            
+            if(temp->random != NULL){
+                 copynode->random=temp->random->next;
+            }else{
+                copynode->random=NULL;
+            }
+            
+            
+            temp=temp->next->next;
+            
+        }
+        
+        //step 3 :- connect next pointer.
+        
+        Node* dummy = new Node(-1);
+        Node* res = dummy;
+        temp=head;
+        
+        while(temp != NULL){
+            res->next=temp->next;
+            temp->next=temp->next->next;
+            res=res->next;
             temp=temp->next;
-
-         }
-
-         return mpp[head];
+        }
+        
+        return dummy->next;
      
     }
 };

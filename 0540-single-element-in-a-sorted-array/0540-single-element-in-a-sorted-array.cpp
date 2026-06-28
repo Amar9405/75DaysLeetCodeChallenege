df5b8e5b3a9085c5d87265d606fd5b347e4ae038
Amar Ramdas Sonawane <amarsonawane940567@gmail.com>
2026-06-28 17:34:43 +0530

    Time: 0 ms (100%), Space: 26.2 MB (68.42%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -2,14 +2,48 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
 
-        //First Approach using XOR
-        int ans=0;
+        // //First Approach using XOR
+        // int ans=0;
 
-        for(int i=0; i<nums.size(); i++){
-            ans^=nums[i];
+        // for(int i=0; i<nums.size(); i++){
+        //     ans^=nums[i];
+        // }
+
+        // return ans;
+
+        int n=nums.size();
+        
+        //edge cases
+        if(n==1) return nums[0];
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[n-1] != nums[n-2]) return nums[n-1];
+
+        int low=1;
+        int high=n-2;
+
+        while(low <=  high){
+
+             int mid=low+(high-low)/2;
+
+            if(nums[mid] != nums[mid+1] && nums[mid]  != nums[mid-1]){
+                return nums[mid];
+            }
+
+
+            //check  for the left part of array
+
+            if(( mid  % 2 == 1 && nums[mid] == nums[mid-1] ) || ( mid % 2 == 0 && nums[mid] == nums[mid+1] )){
+                 low=mid+1;
+            }else{
+                high=mid-1;
+            }
+                 
         }
 
-        return ans;
+        return -1;
+
+
+
         
     }
 };

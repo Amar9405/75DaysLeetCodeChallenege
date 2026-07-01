ae86080262a2299085eec7cb5d0f7e4f0b2713ba
Amar Ramdas Sonawane <amarsonawane940567@gmail.com>
2026-07-01 10:22:54 +0530

    Time: 0 ms (100%), Space: 15 MB (99.47%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -1,19 +1,68 @@
 class Solution {
 public:
+    int FirstOccurrence(vector<int>& nums, int target, int n) {
+        int low = 0, high = n - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                high = mid - 1;   // Search on left side
+            }
+            else if (nums[mid] < target) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    int LastOccurrence(vector<int>& nums, int target, int n) {
+        int low = 0, high = n - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                low = mid + 1;    // Search on right side
+            }
+            else if (nums[mid] < target) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
     vector<int> targetIndices(vector<int>& nums, int target) {
 
-        sort(nums.begin(),nums.end());
+        sort(nums.begin(), nums.end());
+
+        int n = nums.size();
+
+        int first = FirstOccurrence(nums, target, n);
+
+        if (first == -1)
+            return {};
+
+        int last = LastOccurrence(nums, target, n);
 
         vector<int> ans;
 
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i]==target) ans.push_back(i);
+        for (int i = first; i <= last; i++) {
+            ans.push_back(i);
         }
 
-
         return ans;
-
-
-        
     }
 };

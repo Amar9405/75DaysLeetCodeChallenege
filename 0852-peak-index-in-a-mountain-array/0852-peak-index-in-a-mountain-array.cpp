c5444b0766608b0878d8c36ca833b39b5bfcc1d4
Amar Ramdas Sonawane <amarsonawane940567@gmail.com>
2026-07-02 11:29:48 +0530

    Time: 86 ms (0.48%), Space: 63.6 MB (10.74%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+
+        int n=arr.size();
+        int ans=-1;
+
+        for(int i=1; i<n; i++){
+            if(arr[i] > arr[i-1]){
+                ans=i;
+            }
+        }
+
+        return ans;
+
+       
+        
+    }
+};

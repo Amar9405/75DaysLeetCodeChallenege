4487a4ff21a109923c98faf1384190ed32b01ca7
Amar Ramdas Sonawane <amarsonawane940567@gmail.com>
2026-07-16 22:31:16 +0530

    Time: 4 ms (69.17%), Space: 13.5 MB (82.94%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -2,23 +2,36 @@ class Solution {
 public:
     string removeDuplicates(string s) {
 
-        stack<char> st;
+        // stack<char> st;
 
-        for(char ch:s){
-            if(!st.empty() && ch == st.top()){
-                st.pop();
-            }else{
-                st.push(ch);
-            }
-        }
+        // for(char ch:s){
+        //     if(!st.empty() && ch == st.top()){
+        //         st.pop();
+        //     }else{
+        //         st.push(ch);
+        //     }
+        // }
+
+        // string ans="";
+        // while(!st.empty()){
+        //     ans+=st.top();
+        //     st.pop();
+        // }
+
+        // reverse(ans.begin(), ans.end());
+
+        // return ans;
 
         string ans="";
-        while(!st.empty()){
-            ans+=st.top();
-            st.pop();
-        }
 
-        reverse(ans.begin(), ans.end());
+        for(char ch : s){
+
+            if(!ans.size()==0 && ch == ans.back()){
+                ans.pop_back();
+            }else{
+                 ans.push_back(ch);
+            }
+        }
 
         return ans;
         

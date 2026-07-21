cbb0b106d430ef5d6ac3d84b5fbba94eb124de35
Amar Ramdas Sonawane <amarsonawane940567@gmail.com>
2026-07-22 01:28:38 +0530

    Time: 0 ms (100%), Space: 22.6 MB (53.75%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,34 +1,47 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-//    set<int> ss; // O(n log n )
-//    int n=nums.size();
-//    for(int i=0; i<n; i++){
-//         ss.insert(nums[i]); 
-//    }
-//    int index=0;
-//    for(auto st :ss){ //O(n)
-//       nums[index]=st;
-//       index++;
-//    }
-//       return index; 
 
-//space complexity = O(n) for storing unique element 
-//time complexity = O(n log n)+ O(n);
+    //   unordered_set<int> mpp; //set  data structure used only for the . store the unique element
 
-   int n=nums.size();
-   int i=0;
-   for(int j=i+1; j<n; j++){
-       if(nums[j]!=nums[i]){
-         nums[i+1]=nums[j];
-         i++;
-       }
-   }
+    //   int n=nums.size();
 
-   return i+1;
+    //   for(int i=0; i<n; i++){
+    //       mpp.insert(nums[i]);
+    //   } 
 
-   // time complexity of this program is O(n) and sp=O(1).
+    //   int index=0;
+    //   for(auto num : mpp){    
+    //     nums[index]=num;
+    //     index++;      
+    //   }
+
+    //   return index;
+
+    int n=nums.size();
+    
+    int low=0;
+    int high=1;
+    int res=1;
+
+    while(high < n){
+
+         if(nums[high] == nums[high-1]){
+            high++;
+            continue;
+         }
+
+         nums[low+1]=nums[high];
+         low++;
+         high++;
+         res++;
+    }
+
+    return res;
 
 
+
+
+        
     }
 };

9e2a3da770958f27d766fb5ffec4447d88210e1b
Amar Ramdas Sonawane <amarsonawane940567@gmail.com>
2026-07-22 01:33:37 +0530

    Time: 0 ms (100%), Space: 22.7 MB (53.75%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -18,26 +18,40 @@ public:
 
     //   return index;
 
-    int n=nums.size();
+    // int n=nums.size();
     
-    int low=0;
-    int high=1;
-    int res=1;
+    // int low=0;
+    // int high=1;
+    // int res=1;
 
-    while(high < n){
+    // while(high < n){
 
-         if(nums[high] == nums[high-1]){
-            high++;
-            continue;
-         }
+    //      if(nums[high] == nums[high-1]){
+    //         high++;
+    //         continue;
+    //      }
 
-         nums[low+1]=nums[high];
-         low++;
-         high++;
-         res++;
+    //      nums[low+1]=nums[high];
+    //      low++;
+    //      high++;
+    //      res++;
+    // }
+
+    // return res;
+
+    int n=nums.size();
+    int start=0;
+
+    for(int i=1; i<n; i++){
+        if(nums[i] != nums[i-1]){
+            nums[start+1]=nums[i];
+            start++;
+        }
     }
 
-    return res;
+    return start+1;
+
+    
 
 
 

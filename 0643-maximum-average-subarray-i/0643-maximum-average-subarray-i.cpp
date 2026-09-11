a423b5b3de0b1a143b14566bcfe1c7e45cd75e71
Amar Ramdas Sonawane <amarsonawane940567@gmail.com>
2026-09-11 22:38:38 +0530

    Time: 0 ms (100%), Space: 113.9 MB (27.84%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -3,26 +3,30 @@ public:
     double findMaxAverage(vector<int>& nums, int k) {
 
         int n=nums.size();
+
         if(n < k) return 0;
-        double ans = -1000000000.0;
+        
+        double maxavg= -1000000000.0;
+
         double sum=0;
 
-        for(int i=0; i<k;  i++){
-             sum += nums[i];
+        for(int i=0; i<k ; i++){
+            sum+=nums[i];
         }
 
-        ans= max(ans , sum/(k * 1.0));
+
+        maxavg=max(maxavg , sum /(k * 1.0));
 
         for(int i=k; i<n; i++){
-
+            
             sum+=nums[i];
             sum-=nums[i-k];
 
-            ans = max(ans,sum/(k*1.0));
+            maxavg=max(maxavg, sum /(k * 1.0));
+
         }
 
-        return ans;
-
-        
+    return maxavg;
+           
     }
 };

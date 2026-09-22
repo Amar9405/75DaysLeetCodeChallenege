f34b6e6817be36f430dddb8a4be01b81c6c0b1c2
Amar Ramdas Sonawane <amarsonawane940567@gmail.com>
2026-09-22 12:41:03 +0530

    Time: 0 ms (100%), Space: 11.7 MB (27.19%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -2,40 +2,40 @@ class Solution {
 public:
     string minWindow(string s, string t) {
 
-        int minlen=INT_MAX;
-
-        //start index of the substring which have minimum lenght
-        int sIndex=-1;
-
         int n=s.size();
         int m=t.size();
 
         int hash[256]={0};
-        
-        //count the frq. of char in t..
-        for(int c:t){
-            hash[c]++;
+
+        for(char ch:t){
+            hash[ch]++;
         }
 
-        int count=0;
 
         int left=0;
         int right=0;
 
+        int sIndex=-1;
+        int minLength=INT_MAX;
+        int count=0;
+
         while(right < n){
-            
-            if(hash[s[right]] >  0){
+
+            if(hash[s[right]] > 0){
                 count++;
             }
             hash[s[right]]--;
 
-            while(count == m){
-                if(right - left + 1 < minlen){
-                    minlen= right - left + 1;
-                    sIndex =left;
+            while(count==m){
+
+                if(right-left+1 < minLength){
+                    minLength=right-left+1 ;
+                    sIndex=left;
                 }
-                
+
+
                 hash[s[left]]++;
+
 
                 if(hash[s[left]] > 0){
                     count--;
@@ -49,8 +49,11 @@ public:
         }
 
 
-        return (sIndex == -1) ? "" : s.substr(sIndex,minlen);
-
+        if(sIndex==-1){
+            return "";
+        }else{
+            return s.substr(sIndex,minLength);
+        }   
         
     }
 };
